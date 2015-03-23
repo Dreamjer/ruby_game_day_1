c80f1a3911a5168c1e22daef25591eb7e7856be2
Jerome Tessier <jerome.tessier@wandis.com>
2015-03-23 17:23:44 +0100

    Passage en attr_accessor

--- player.rb
+++ player.rb
@@ -1,17 +1,8 @@
 class Player
-	attr_reader :nom , :ligne , :colone
+	attr_accessor :nom , :ligne , :colone
 	def initialize(nom,ligne,colone)
 		@nom = nom
 		@ligne = ligne
 		@colone = colone
-	end
-	def nom=(new_nom)
-		@ligne = new_nom
-	end
-	def ligne=(new_ligne)
-		@ligne = new_ligne
-	end
-	def colone=(new_colone)
-		@colone = new_colone
 	end
 end

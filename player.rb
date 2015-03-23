3b70cfec586fbebdb79014cd706b72a253abf1ad
Jerome Tessier <jerome.tessier@wandis.com>
2015-03-23 16:33:39 +0100

    Creation du fichier de class

---
+++ player.rb
@@ -0,0 +1,16 @@
+class Player
+	def initialize(nom,ligne,colone)
+		@nom = nom
+		@ligne = ligne
+		@colone = colone
+	end
+  def nom
+  	@nom
+	end
+	def ligne
+  	@ligne
+	end
+	def colone
+  	@colone
+	end
+end

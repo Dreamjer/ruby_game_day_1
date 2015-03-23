22252a220aa5d71a7fa7f84aea757f7b84e6f296
Jerome Tessier <jerome.tessier@wandis.com>
2015-03-23 14:57:04 +0100

    Ajout coordonnees pour placer la position dans le tableau

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,19 +1,24 @@
-ligne = 1
-while ligne < 6
-	col = 1
-	while col < 6
-		print "X "
-		col += 1
-	end
-	print "\n"
-	ligne += 1
-end
+#ligne = 1
+#while ligne < 6
+#	col = 1
+#	while col < 6
+#		print "X "
+#		col += 1
+#	end
+#	print "\n"
+#	ligne += 1
+#end
+#
+#puts "------------"
 
-puts "------------"
-
+posl,posc = 2 , 5
 for line in (1..5)
 	for col in (1..5)
-		print "X "
+		if posl == line && posc == col
+			print "P "
+		else
+			print "X "
+		end
 	end
 	print "\n"
 end
